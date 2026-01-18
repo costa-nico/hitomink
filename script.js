72da7320ce09de8abd0637f783f1aa6cc70e6146
CJ <cjf8140@gmail.com>
2026-01-19 03:08:39 +0900

    없는듯?들어가보셈?있을수도?

--- script.js
+++ script.js
@@ -99,7 +99,7 @@ function createBotMessage(message, loading=false) {
             // 이미지 썸네일 표시
             const img = document.createElement('img');
             img.src = thumbnailUrl;
-            img.alt = '들어가보셈';
+            img.alt = '없는듯?들어가보셈?있을수도?';
             img.className = 'thumbnail-image';
             chatContainer.appendChild(img);
 
@@ -115,7 +115,7 @@ function createBotMessage(message, loading=false) {
         const thumbnailUrl = `https://soujpa.in/start/${message}/${message}_0.avif`;
         const img = document.createElement('img');
         img.src = thumbnailUrl;
-        img.alt = '들어가보셈';
+        img.alt = '없는듯?들어가보셈?있을수도?';
         img.className = 'first-page';
         chatContainer.appendChild(img);
     }
